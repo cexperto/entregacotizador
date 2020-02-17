96390caf022276de626d3cb2c801836ba00ad9c5
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-17 15:43:40 -0500

    Update correoA.php

--- correoA.php
+++ correoA.php
@@ -1,7 +1,7 @@
 require 'vendor/autoload.php';
 $sendgrid = new SendGrid("SENDGRID_APIKEY");
 $email    = new SendGrid\Email();
-
+echo "ji";
 $email->addTo("rattman.c@gmail.com")
       ->setFrom("latiendadelmoterobogota@gmail.com")
       ->setSubject("Sending with SendGrid is Fun")
