91ff7014cc2d9d0081f64d319a0d0e9a2ba4b327
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-17 16:27:15 -0500

    Update a.php

--- a.php
+++ a.php
@@ -39,16 +39,6 @@ if($rowCount > 0)
             echo "$pricetotal1 pago en 30 dias<br></h4></font>
                         
             ";
-require 'vendor/autoload.php';
-$sendgrid = new SendGrid("SENDGRID_APIKEY");
-$email    = new SendGrid\Email();
-echo "ji";
-$email->addTo("rattman.c@gmail.com")
-      ->setFrom("latiendadelmoterobogota@gmail.com")
-      ->setSubject("Sending with SendGrid is Fun")
-      ->setHtml("and easy to do anywhere, even with PHP");
-
-$sendgrid->send($email);
         }
         else{
                 echo "<script>alert('error al enviar');</script>";
