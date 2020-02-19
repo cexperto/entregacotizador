dd49a2f929604d307a51eaf16438a193cb8b6871
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-19 01:38:57 -0500

    Update correoA.php

--- correoA.php
+++ correoA.php
@@ -1,20 +1,37 @@
 <?php
-// If you are using Composer
 require 'vendor/autoload.php';
 
-// If you are not using Composer (recommended)
-// require("path/to/sendgrid-php/sendgrid-php.php");
 
-$from = new SendGrid\Email(null, "rattman.c@gmail.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "sipuedeshazloya@gmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+    $key ="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";
+    // using SendGrid's PHP Library
+    // https://github.com/sendgrid/sendgrid-php
+    require 'vendor/autoload.php'; // If you're using Composer (recommended)
+    // Comment out the above line if not using Composer
+    // require("./sendgrid-php.php");
+    // If not using Composer, uncomment the above line
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("rattman.c@gmail.com", "Example User");
+    $email->setSubject("Sending with SendGrid is Fun");
+    $email->addTo("sipuedeshazloya@gmail.com", "Example User");
+    $email->addContent(
+        "text/plain", "and easy to do anywhere, even with PHP"
+    );
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+    
+    
+$sendgrid = new \SendGrid($key);
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 
-$apiKey = getenv('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');
-$sg = new \SendGrid($apiKey);
+        
+    
+    ?>
 
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
