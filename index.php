d09c34c83b08588f7ffbd3ecbe322969d813828d
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-19 01:37:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -119,39 +119,3 @@ style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
 <script src="js/marca.js"></script>
 <script src="js/referencia.js"></script>
 <script src="js/soat.js"></script>
-<?php
-require 'vendor/autoload.php';
-
-if(isset($_POST['enviar'])){
-        $key ="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";
-    // using SendGrid's PHP Library
-    // https://github.com/sendgrid/sendgrid-php
-    require 'vendor/autoload.php'; // If you're using Composer (recommended)
-    // Comment out the above line if not using Composer
-    // require("./sendgrid-php.php");
-    // If not using Composer, uncomment the above line
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom("rattman.c@gmail.com", "Example User");
-    $email->setSubject("Sending with SendGrid is Fun");
-    $email->addTo("sipuedeshazloya@gmail.com", "Example User");
-    $email->addContent(
-        "text/plain", "and easy to do anywhere, even with PHP"
-    );
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-    
-    
-$sendgrid = new \SendGrid($key);
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-}
-
-        
-    }
-    ?>
