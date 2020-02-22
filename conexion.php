c0d96a8578cef920d9c0d0f928428659df9b524a
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-22 10:59:11 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -9,16 +9,24 @@ if($conexion){
     //echo "exito";
 }else{
     echo "pailas";
-}*/
+}
 $dbHost = 'remotemysql.com';
 
 $dbUsername = 'gu6EqlTj8p';
 
 $dbPassword = 'sqYM6pwdQp';
 
-$dbName = 'gu6EqlTj8p';
+$dbName = 'gu6EqlTj8p';*/
+$dbHost = '198.136.62.62';
+
+$dbUsername = 'ejxhyjxs_motos';
+
+$dbPassword = 'pl$wbamqWA.2';
+
+$dbName = 'ejxhyjxs_motos';
 
 
+198.136.62.62
 //Conectamos y seleccionamos la base de datos
 
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
