dc2454e83b3d30908bbf898172afc74ac4d194da
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-24 23:17:13 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -25,7 +25,7 @@ $dbUsername = 'gu6EqlTj8p';
 $dbPassword = 'sqYM6pwdQp';
 
 $dbName = 'gu6EqlTj8p';*/
-$dbHost = '186.154.39.8';
+$dbHost = 'hd-5110';
 $dbUsername = 'ejxhyjxs_motos';
 $dbPassword = 'pl$wbamqWA.2';
 $dbName = 'ejxhyjxs_motos';
